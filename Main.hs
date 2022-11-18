c8fd7402c977a66918fb5f03690035fc7fac586c
Andrii Maliuta <quadr988@gmail.com>
2022-11-18 20:38:04 +0200

    http

--- Main.hs
+++ Main.hs
@@ -3,13 +3,26 @@
 
 module Main where
 
-import Database.PostgreSQL.Simple
 import Control.Monad
 import Data.Foldable
+import Data.String
 import Data.Convertible
+import Data.List
 import Data.Time.Clock.POSIX
 import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Char8 as S8
+import qualified Data.Yaml as Yaml
+import Database.PostgreSQL.Simple
+import Network.HTTP.Client.Conduit
 import Network.HTTP.Simple
+import Data.Aeson (Value)
+import Text.HTML.TagSoup
+import Text.StringLike(StringLike)
+
+
+main :: IO ()
+main = do
+  myParser
 
 
 data Page = Page { id :: Integer, 
@@ -20,27 +33,32 @@ data Page = Page { id :: Integer,
                   last_updated :: String,
                   parent_id :: Integer}
 
+myParser :: IO ()
+myParser = do
+  let wikiUrl = "https://en.wikipedia.org/wiki/Main_Page"
+  let httpBinUrl = "http://httpbin.org/get"
 
-parseMy :: IO ()
-parseMy = do
-  -- https://hackage.haskell.org/package/http-client-0.7.13.1/docs/Network-HTTP-Client.html#t:Response
-  let wikiUrl = "http://en.wikipedia.org/wiki/Main_Page"
-  response <- httpLBS "http://httpbin.org/get"
-  putStrLn $ "The status code was: " ++ show (getResponseStatusCode response)
-  print $ getResponseHeader "Content-Type" response
-  L8.putStrLn $ getResponseBody response
+  response <- httpLBS wikiUrl
+  let ctypeHeader = getResponseHeader "Content-Type" response
+  let respCode = getResponseStatusCode response
+  let respBody = getResponseBody response
+--  S8.putStrLn $ Yaml.encode (getResponseBody response :: L8.ByteString)
+  let tags = parseTags respBody
+  --let divs = filter  (filter isTagOpen tags)
+  --let divs = filter isInfixOf "div" ( filter isTagOpen tags )
+  --let divs = filter strEq "div" ( filter isTagOpen tags )
+  let divs = sections (~== "div") 
+
+  print divs
 
 
 get_pages_psql :: IO ()
 get_pages_psql = do
-  conn <- connectPostgreSQL "host=167.235.52.214 dbname=wiki1 user=dev password="
+  conn <- connectPostgreSQL "host=167.235.52.214 dbname= user=dev password="
   pages :: [(String, String)]  <- query_ conn "select title, space_key from pages"  
   mapM_ print pages   
   --for_ [1..10] (print (pages !! 2)
   --for_ pages $ print $ show filterPages
     --where filterPages page = 4 `elem` (fst page)
-  
 
-main :: IO ()
-main = do
-  parseMy
+
